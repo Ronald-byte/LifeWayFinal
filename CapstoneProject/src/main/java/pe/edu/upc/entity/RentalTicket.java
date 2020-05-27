a5fd0e48e33f6ccf466fcdb4119dde047f1a032a
djimenef <david.jf1596@gmail.com>
2020-05-26 20:23:46 -0500

    RentalTicket atributtes

--- CapstoneProject/src/main/java/pe/edu/upc/entity/RentalTicket.java
+++ CapstoneProject/src/main/java/pe/edu/upc/entity/RentalTicket.java
@@ -1,39 +1,64 @@
 package pe.edu.upc.entity;
 import java.io.Serializable;
 import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 @Entity
 @Table(name="RentalTicket")
 public class RentalTicket implements Serializable {
  private static final long serialVersionUID = 1L;
+ 
  @Id
  @GeneratedValue(strategy = GenerationType.IDENTITY)
  private int idRental;
  
+ @Column(name = "DateRental")
  private Date rentalDate;
  
+ @Column(name = "NrHours", nullable = false, length = 30)
  private int numberHours;
  
- private int numberCost;
+ @Column(name = "CostRate", nullable = false, length = 30)
+ private int costRate;
  
+ @ManyToOne
+ @JoinColumn(name = "idCustomer")
+ private Customer customer;
+
+ @Column(name = "TotalCost", nullable = false, length = 30)
  private int totalCost;
 
-public RentalTicket(int idRental, Date rentalDate, int numberHours, int numberCost, int totalCost) {
-	super();
-	this.idRental = idRental;
-	this.rentalDate = rentalDate;
-	this.numberHours = numberHours;
-	this.numberCost = numberCost;
-	this.totalCost = totalCost;
-}
+ @ManyToOne
+ @JoinColumn(name = "idBike")
+ private Bike bike;
+ 
+ @ManyToOne
+ @JoinColumn(name = "idEmployee")
+ private Employee employee;
 
 public RentalTicket() {
 	super();
 	// TODO Auto-generated constructor stub
+}
+
+public RentalTicket(int idRental, Date rentalDate, int numberHours, int costRate, Customer customer, int totalCost,
+		Bike bike, Employee employee) {
+	super();
+	this.idRental = idRental;
+	this.rentalDate = rentalDate;
+	this.numberHours = numberHours;
+	this.costRate = costRate;
+	this.customer = customer;
+	this.totalCost = totalCost;
+	this.bike = bike;
+	this.employee = employee;
 }
 
 public int getIdRental() {
@@ -60,12 +85,20 @@ public void setNumberHours(int numberHours) {
 	this.numberHours = numberHours;
 }
 
-public int getNumberCost() {
-	return numberCost;
+public int getCostRate() {
+	return costRate;
 }
 
-public void setNumberCost(int numberCost) {
-	this.numberCost = numberCost;
+public void setCostRate(int costRate) {
+	this.costRate = costRate;
+}
+
+public Customer getCustomer() {
+	return customer;
+}
+
+public void setCustomer(Customer customer) {
+	this.customer = customer;
 }
 
 public int getTotalCost() {
@@ -75,6 +108,25 @@ public int getTotalCost() {
 public void setTotalCost(int totalCost) {
 	this.totalCost = totalCost;
 }
+
+public Bike getBike() {
+	return bike;
+}
+
+public void setBike(Bike bike) {
+	this.bike = bike;
+}
+
+public Employee getEmployee() {
+	return employee;
+}
+
+public void setEmployee(Employee employee) {
+	this.employee = employee;
+}
+ 
+ 
+ 
  
  
  
